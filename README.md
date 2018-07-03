fa5cb64083c1682e0650e9808bda3822e43908a7
chenkaven <chenkaven@chenkavendeMacBook-Pro.local>
2018-07-03 15:04:52 +0800

    修改icontools

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # python_mac
 基于python开发
+buildtool: 打包ios
+codetools:添加垃圾代码工具
+icontools:工具
+
--- iconTool/autoicon.py
+++ iconTool/autoicon.py
@@ -5,60 +5,88 @@
 import argparse;
 import os,os.path,sys,shutil;
 from PIL import Image;
+import threading
 
-
-# 输入图片地址
-target_Desktop_path = os.path.join(os.path.expanduser("~"),  'Desktop/imageView/Icon-512.png');
-
-#输出图片的地址
-target_iconRGB_path = os.path.join(os.path.expanduser("~"),  'Desktop/imageView/icon_RGB/');
-target_iconRGBA_path = os.path.join(os.path.expanduser("~"), 'Desktop/imageView/icon_RGBA/');
-
-#Android输出地址：
-target_Android_path = os.path.join(os.path.expanduser("~"),  'Desktop/imageView/icon_Android/');
-#ios的输出地址：
-target_iOS_path = os.path.join(os.path.expanduser("~"),  'Desktop/imageView/icon_ios/');
-
-target_ImageSet_path = os.getcwd()+os.path.sep+'ImagesSet';
 #iconName:
 iconName = 'icon.png';
-#androidImgName = ['ic_launcher.png','ic_launcher_round.png'];
+sIcon_path = os.getcwd()+os.path.sep+'ImageFold';
+#模板;
+target_ImageSet_path = os.getcwd()+os.path.sep+'ImagesSet';
+# 输入图片地址
+target_Desktop_path = os.path.join(os.path.expanduser("~"),'Desktop/');
 
 
-# serchIconPath = '/Users/apple/Desktop/商务需求/三国卡牌-英瓷-6.20日传/ICON.jpg';
+#Android输出地址：
+target_Android_path = target_Desktop_path+'imageView/icon_android/';
+
+#ios输出地址：
+target_iconRGB_path = target_Desktop_path+ 'imageView/icon_RGB/';
+target_iOS_path = target_Desktop_path+ 'imageView/icon_ios/';
+target_outFile_path = target_iOS_path+'AppIcon.appiconset/';
 
 def encodeChincese(msg):
-	type = sys.getfilesystemencoding();
-	return msg.decode('utf-8').encode(type);
-	pass
-
+    type = sys.getfilesystemencoding();
+    return msg.decode('utf-8').encode(type);
+    pass
+#创建二维数组
+def creatiOSList():
+    try:
+        # 初始化图片数组
+        image_DICT = [['20','icon-20-ipad.png'],
+                      ['40', 'icon-20@2X-ipad.png'],
+                      ['40','icon-20@2x.png'],
+                      ['60','icon-20@3x.png'],
+                      ['29', 'icon-29-ipad.png'],
+                      ['58','icon-29@2x-ipad.png'],
+                      ['29', 'icon-29.png'],
+                      ['58','icon-29@2x.png'],
+                      ['87','icon-29@3x.png'],
+                      ['40','icon-40-ipad.png'],
+                      ['80', 'icon-40@2x-ipad.png'],
+                      ['80','icon-40@2x.png'],
+                      ['120','icon-40@3x.png'],
+                      ['50', 'icon-50-ipad.png'],
+                      ['100', 'icon-50@2x-ipad.png'],
+                      ['57', 'icon-57.png'],
+                      ['114', 'icon-57@2x.png'],
+                      ['120','icon-60@2x.png'],
+                      ['180','icon-60@3x.png'],
+                      ['72', 'icon-72-ipad.png'],
+                      ['144', 'icon-72@2x-ipad.png'],
+                      ['76','icon-76-ipad.png'],
+                      ['152','icon-76@2x-ipad.png'],
+                      ['167','icon-83.5@2x-ipad.png'],
+                      ['1024','icon-1024.png']];
+        return image_DICT;
+    except Exception as e:
+        print(e);
 ########################################
 #1.打开图片
 def	open_imgFile(sFiledir):
-	im = Image.open(sFiledir);
-	im.load();
-	return im;
+    im = Image.open(sFiledir);
+    im.load();
+    return im;
 
 #2.检查图像通道
 def check_ImgModel(sFiledir):
-	im = open_imgFile(sFiledir);
-	return im.mode;	
+    im = open_imgFile(sFiledir);
+    return im.mode;
 
 #3、修改图片尺寸
 def modify_ImgSize(sInFile,sWidth,sHeight):
-	im = open_imgFile(sInFile);
-	(x,y)=im.size;
-	if (sWidth != 0 and sHeight !=0):
-		x=sWidth;
-		y=sHeight;
-	im = im.resize((x,y),Image.ANTIALIAS);
-	return im;
+    im = open_imgFile(sInFile);
+    (x,y)=im.size;
+    if (sWidth != 0 and sHeight !=0):
+        x=sWidth;
+        y=sHeight;
+    im = im.resize((x,y),Image.ANTIALIAS);
+    return im;
 
 #4、保存白底转透明;
 def save_ImgRGBAFile(sInFile,sOutFile,sWidth,sHeight):
     imgA = modify_ImgSize(sInFile,sWidth,sHeight);
- 	# 使用白色来填充背景
-  	# (alpha band as paste mask). 
+    # 使用白色来填充背景
+    # (alpha band as paste mask).
     imgA = imgA.convert("RGBA");
     datas = imgA.getdata();
     newData = list();
@@ -80,74 +108,144 @@ def save_ImgRGBFile(sInFile,sOutFile,sWidth,sHeight):
     p.save(sOutFile, 'png', quality=100);
     p.close();
 
-
-
-
-    
-
-########################################
+#创建或更新文件/文件夹
 def update_ImgPath(sPath):
-	isPath = os.path.exists(sPath);
-	isFile = os.path.isfile(sPath);
-	isDir = os.path.isdir(sPath);
-	if (isPath == True):
-	    shutil.rmtree(sPath);
-	#print("######\n%s\n不存在则删除重建\n######"%(sPath));
-	if(isFile ==True):
-		#创建单个文件
-		os.mknod(sPath);
-	else:
-		#创建多个目录
-		os.makedirs(sPath);
+    isPath = os.path.exists(sPath);
+    isFile = os.path.isfile(sPath);
+    isDir = os.path.isdir(sPath);
+    if (isPath == True):
+        shutil.rmtree(sPath);
+    #print("######\n%s\n不存在则删除重建\n######"%(sPath));
+    if(isFile ==True):
+        #创建单个文件
+        os.mknod(sPath);
+    else:
+        #创建多个目录
+        os.makedirs(sPath);
+########################################
 
 
-#查找图片路径;
-def check_ImgPath():
-	global target_Desktop_path,target_iconRGB_path,iconName;
-	if (check_ImgModel(target_Desktop_path)) == 'RGBA':
-	    print('透明转为白底');
-	    update_ImgPath(target_iconRGB_path);
-	    sIconName = target_iconRGB_path + iconName;
-	    save_ImgRGBFile(target_Desktop_path,sIconName,0,0);
-	else:
-	    print('白底转为透明');
-	    update_ImgPath(target_iconRGBA_path);
-	    sIconName = target_iconRGBA_path + iconName;
-	    save_ImgRGBAFile(target_Desktop_path,sIconName,0,0);
-	#创建Android图标
-	creat_AndroidIcon();
+
+#搜索当前路径/imageView下的所有图片；
+def serach_ImgPath():
+    global  sIcon_path;
+    image_list = [];
+    for root, dirs, files in os.walk(sIcon_path):
+        # print("2==", files,dirs);
+        for name in files:
+            ext = os.path.splitext(name)[1][1:];
+            # print("3==", ext);
+            if (ext.lower() == 'png'
+                or ext.lower() == 'jpg'
+                or ext.lower() == 'PNG'):
+                # if尾部为.png，...
+                sPath = root + os.sep + name;
+                image_list.append(sPath);
+                new_img = open_imgFile(sPath);
+                (w,h)= new_img.size;
+                if(h != w):
+                   h = w;
+                   new_img = new_img.resize((w, h), Image.ANTIALIAS);
+                   new_img.save(sPath, 'png', quality=100);
+                else:
+                   print("图片正常，无须裁剪");
+    #print("image_list:",image_list);
+    return image_list;
+
+#制作安卓的图片
+def markAndroidImgFile():
+    global iconName,target_Android_path;
+    update_ImgPath(target_Android_path);
+    mackDir = serach_ImgPath();
+    if (len(mackDir) == 2):
+        base_img = Image.open(mackDir[0]);
+        cor_img = Image.open(mackDir[1]);
+        source = cor_img.convert('RGB');
+        base_img.paste(source, mask=cor_img);
+        # 创建桌面安卓文件夹
+        sOutFile = target_Android_path+iconName;
+        base_img.save(sOutFile, 'png', quality=100);
+    else:
+        sOutFile = target_Android_path + iconName;
+        base_img = Image.open(mackDir[0]);
+        base_img.save(sOutFile, 'png', quality=100);
+        print("Success：无角标,合成安卓图标");
+    pass;
+
+#检查是否透明通道
+def translate_ImgModel():
+    global  target_Android_path,target_iconRGB_path,iconName;
+    update_ImgPath(target_iconRGB_path);
+    hImg = target_Android_path +iconName;
+    if(check_ImgModel(hImg) == 'RGBA'):
+       print("透明转为白底的");
+       sIconName = target_iconRGB_path +iconName;
+       save_ImgRGBFile(hImg,sIconName,0,0);
+    else:
+        print("白底的/不透明的");
+    pass;
 
 
-def creat_AndroidIcon():
-	update_ImgPath(target_Android_path);
-	creat_iOSIcon();
-
-
+# 创建ios项目
 def creat_iOSIcon():
-	#创建ios项目
-	# update_ImgPath(target_iOS_path);
-	isSetExit = os.path.exists(target_ImageSet_path);
-	isIosExit = os.path.exists(target_iOS_path);
-	if(isIosExit == True):
-		#print("判断存在苹果的icon目录，则删除");
-		shutil.rmtree(target_iOS_path);
-
-	if (isSetExit == True):
-		print("判断存在苹果的icon,则复制到苹果icon目录");
-		shutil.copytree(target_ImageSet_path,target_iOS_path);
-
-
+    isSetExit = os.path.exists(target_ImageSet_path);
+    isIosExit = os.path.exists(target_iOS_path);
+    if(isIosExit == True):
+        #print("判断存在苹果的icon目录，则删除");
+        shutil.rmtree(target_iOS_path);
+    if (isSetExit == True):
+        print("复制到苹果icon目录");
+        shutil.copytree(target_ImageSet_path,target_iOS_path);
+	#开始做操作了；
+	print("iOS_Icon开始秀操作了");
+	mainiOSIcon();
 
 
 
+# 正式开始制作iosIcon
+def mainiOSIcon():
+    print("****======Start=========***");
+    imgDict = creatiOSList();
+	# print(i, imgDict[i]);
+    threads = []  # 多线程
+    for i in range(0, len(imgDict)):
+        #遍历参数
+        for j in range(0, len(imgDict[i])):
+            #print("图片~j",j,imgDict[i][j]);
+            if j ==0:
+                mWidth= imgDict[i][j];
+            else:
+                mReName=imgDict[i][j];
+        thread1 = threading.Thread(target=convertIcon,args=(i+1,mWidth,mReName));
+        thread1.start();
+    print("****======End=========***");
+    print ("current has %d threads" % (threading.activeCount() - 1));
 
 
-	    
-    		
+# icon转换
+def convertIcon(i,width, reName):
+    try:
+        #1、获取安卓的桌面图标
+        inPath = target_iconRGB_path + iconName;
+        img = Image.open(inPath);
+        width = int(width);
+        height = width;
+        new_img = img.resize((width, height), Image.ANTIALIAS);
+        # 2、获取ios图标的桌面文件夹
+        outPath = target_outFile_path + reName;
+        print('***'+"第"+str(i)+"张图片正在合成...");
+        #print ('start waiting:', time.strftime('%H:%M:%S'));
+        new_img.save(outPath, quality=100);
+      #time.sleep(1);
+       #print('stop waiting', time.strftime('%H:%M:%S'));
+    except Exception as e:
+        print(e);
 
 
 
 if __name__ == '__main__':
     #检查图片通道并保存;
-	#check_ImgPath();
-	global target_Desktop_path,target_iconRGB_path,iconName;
+    markAndroidImgFile();
+    translate_ImgModel();
+    creat_iOSIcon();
+
